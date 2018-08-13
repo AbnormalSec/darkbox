b025bd09207340dcf638628d53c5342c66a9ff74
Skyler Curtis <skylerr.curtis@gmail.com>
2018-08-13 14:27:27 -0600

    rough version of base64

---
+++ commands/base64.py
@@ -0,0 +1,40 @@
+""" darkbox template for a command """
+
+import argparse
+from base64 import b64encode, b64decode
+from textwrap import wrap
+
+class base64:
+    def __init__(self):
+        self.version = '0.0.0'
+    
+    def get_parser(self):
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument('-d', '--decode', action='store_true')
+        #parser.add_argument('-i', '--ignore-garbage', action='store_true')
+        parser.add_argument('-w', '--wrap', type=int, default=76)
+        parser.add_argument('-v', '--version', default=False, action='store_true')
+        parser.add_argument("file", nargs="?")
+        return parser
+   
+    def run(self):
+        parser = self.get_parser()
+        args = vars(parser.parse_args())
+        if args['version']:
+            print('darkbox {cls} v{version}'.format(
+                cls=self.__class__.__name__,
+                version=self.version))
+            return
+
+        with open(args['file'], 'rb') as f:
+            in_data = f.read()
+
+        if args['decode']:
+            in_data = in_data.replace(b"\n",b"")
+            print(b64decode(in_data).decode("utf-8"), end='')
+
+        else:
+            print('\n'.join(wrap(b64encode(in_data).decode("utf-8"), args['wrap'])))
+
+
