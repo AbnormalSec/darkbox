dcd51d190e5f213cec05d01a497cf78a1d40506c
vesche <austinjackson892@gmail.com>
2018-08-17 16:29:46 -0400

    Update sha256sum a bit

--- commands/sha256sum.py
+++ commands/sha256sum.py
@@ -1,16 +1,18 @@
 """ darkbox sha256sum command """
 
-import hashlib
 from .template import Command
+
+import hashlib
 
 
 class sha256sum(Command):
     def __init__(self):
-        self.version = '0.0.2'
+        self.version = '0.0.3'
         self.algo = 'sha256'
     
     def get_parser(self):
-        parser = super().get_parser(description='darkbox {}sum'.format(self.algo))
+        parser = super().get_parser(description=
+            'darkbox {}sum'.format(self.algo))
         parser.add_argument('files', nargs='*', help='input file(s)')
         return parser
     
@@ -22,14 +24,14 @@ class sha256sum(Command):
             self.get_parser().print_help()
             return
 
-        for i in files:
+        for file_path in files:
             try:
-                with open(i, 'rb') as f:
+                with open(file_path, 'rb') as f:
                     hash_ret = hashlib.new(self.algo, f.read())
-                print('{} {}'.format(hash_ret.hexdigest(), i))
+                print('{} {}'.format(hash_ret.hexdigest(), file_path))
 
             except IsADirectoryError:
-                self.directory_error(i)
+                self.directory_error(file_path)
 
             except FileNotFoundError:
-                self.file_not_found_error(i)
+                self.file_not_found_error(file_path)
