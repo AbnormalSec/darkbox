08500cb05645f963179da472d75ef3b73b0647af
deadpix3l <skylerr.curtis@gmail.com>
2018-09-19 18:42:42 +0000

    new tool (beta): wc

---
+++ darkbox/commands/wc.py
@@ -0,0 +1,55 @@
+"""darkbox cat command"""
+
+from darkbox.commands.template import Command
+
+
+class wc(Command):
+    """darkbox wc
+
+    count characters or lines in files
+
+    Designed to be similar to wc from GNU coreutils.
+    Resource: https://www.gnu.org/software/coreutils/wc
+    """
+
+    def __init__(self):
+        self.version = '0.0.1'
+    
+    def get_parser(self):
+        parser = super().get_parser(description='darkbox wc')
+        parser.add_argument('-c', '--bytes', action='store_true')
+        parser.add_argument('-l', '--lines', action='store_true')
+        parser.add_argument('-w', '--words', action='store_true')
+        parser.add_argument('files', nargs='+', help='input file')
+        return parser
+
+    def run(self):
+        args = self.get_args()
+        
+        # if none are chosen, then choose all (per GNU behavior)
+        if not any([args['lines'], args['bytes'], args['words']]):
+            args['lines'] = True
+            args['bytes'] = True
+            args['words'] = True
+        
+        totals = {'words': 0, 'lines': 0, 'chars': 0}
+        
+        for i in args['files']:
+            try:
+                with open(i, 'rb') as f:
+                    file_metrics = {'lines': 0, 'bytes': 0, 'words': 0}
+                    
+                    curr_char = f.read(1)
+                    while curr_char != b'':
+                        file_metrics['bytes'] += 1
+                        if curr_char == b'\n': file_metrics['lines'] += 1
+                        if curr_char in b'\n\t ': file_metrics['words'] += 1
+                        curr_char = f.read(1)
+                        
+                print("{} {} {} {}".format(file_metrics['lines'], file_metrics['words'], file_metrics['bytes'], i))
+
+            except FileNotFoundError:
+                self.file_not_found_error(i)
+
+            except IsADirectoryError:
+                self.directory_error(i)
