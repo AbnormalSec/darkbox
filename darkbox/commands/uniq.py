e8f26bb52354ec2dc6135b4b687f9e5b53900837
deadpix3l <skylerr.curtis@gmail.com>
2018-09-20 22:47:15 +0000

    uniq: added -r, -i and -u options

--- darkbox/commands/uniq.py
+++ darkbox/commands/uniq.py
@@ -19,9 +19,9 @@ class uniq(Command):
         parser = super().get_parser(description='darkbox uniq')
         parser.add_argument("-c", "--count", action="store_const", const="{cnt} {str}", default="{str}", help="prefix lines by the number of occurrences")
         parser.add_argument("-d", "--repeated", action="store_true", help="only print duplicate lines")
-        parser.add_argument("-i", "--ignore-case", action="store_true", help="ignore differences in case when comparing")
+        parser.add_argument("-i", "--ignore-case", action="store_const", const=str.lower, default=str, help="ignore differences in case when comparing")
         parser.add_argument("-u", "--unique", action="store_true", help="only print unique lines")
-        parser.add_argument("-z", "--zero-terminated", action="store_true", help="end lines with 0 byte, not newline")
+        #parser.add_argument("-z", "--zero-terminated", action="store_true", help="end lines with 0 byte, not newline")
         parser.add_argument("file", help="input file")
         return parser
 
@@ -29,6 +29,7 @@ class uniq(Command):
         args = self.get_args(args)
     
         fmtstr = args['count']
+        compare = args['ignore_case']
     
         try:
             with open(args['file'], 'r') as f:
@@ -36,20 +37,23 @@ class uniq(Command):
                 old_line = f.readline()
                 count = 1
                 for line in f:
-                    if line != old_line:
-                        print(fmtstr.format(cnt=count, str=old_line), end='')
+                    if compare(line) != compare(old_line):
+                        if not (count == 1 and args["repeated"]):
+                            if not (count > 1 and args['unique']):
+                                print(fmtstr.format(cnt=count, str=old_line), end='')
                             
                         count = 1
+                        old_line = line
                     
                     else:
                         count += 1
-                    
-                    old_line = line
-    
-                print(old_line, end='')
+
+            if not (count == 1 and args["repeated"]):
+                if not (count > 1 and args['unique']):
+                    print(fmtstr.format(cnt=count, str=old_line), end='')
                 
         except FileNotFoundError:
-            self.file_not_found_error(i)
+            self.file_not_found_error(args['file'])
 
         except IsADirectoryError:
-            self.directory_error(i)
+            self.directory_error(args['file'])
